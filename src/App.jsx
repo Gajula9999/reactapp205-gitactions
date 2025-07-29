266206285a4249397a425a7e907c2072374cebb3
VEERA <veera@gmail.com>
2025-07-29 12:37:21 +0530

    app.jsx is updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div className="container">
       <h1>Welcome to My React Project - S205</h1>
       <p>CI/CD and Cloud DevOps</p>
-      <p>This is a simple homepage deployed on GitHub  Actions  Pages using Vite </p>
+      <p>DONE WITH GIT PAGES AND GIT ACTIONS  </p>
     </div>
   );
 }
